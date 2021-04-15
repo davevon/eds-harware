0eeab806f10d10926480a979e1024b868d66a178
davevon <61807777+davevon@users.noreply.github.com>
2021-04-15 15:44:51 -0500

    added pictures folder.added a Sales section.

---
+++ hardwarestore/Controllers/Edhardware_Sales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PointofSaleapplication.Controllers
+{
+    public class Edhardware_Sales : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
